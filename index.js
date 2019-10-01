1a40242a77833cc66f119d90e618ed03e321ae62
Guilherme Otsu <guiotsu@gmail.com>
2019-09-30 21:51:53 -0300

    config.json

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const { Client, Attachment } = require('discord.js');
+
+const { discordToken } = require('./config.json');
 
 client.on('ready', function() { 
     console.log('Ready');
@@ -8,8 +11,23 @@ client.on('ready', function() {
 client.on('message', message => { 
     if(message.content === '!ping')
     {
-        message.reply('pong');
+        message.reply('com .reply: pong');
+        message.channel.send('sem reply: pong');
+    } else if(message.content === '!server')
+    {
+        message.channel.send(
+        `O nome do servidor é ${message.guild.name}`
+        )
+    } else if(message.content === '!rip') { 
+        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
+        message.channel.send(attachment);
+    } else if (message.content.startsWith("!maiuscula"))
+    {
+        const args = message.split(' ');
+        const command = args.shift();  
+        const response = args.join(' ');
+        message.channel.send(response.toUpperCase);
     }
 });
 
-client.login('NjI4MzYwNTQyNzE2NjkwNDYz.XZKPVQ.YtZzUztZcMQwi0UW5aMrNWFVNwg');
+client.login(discordToken);
